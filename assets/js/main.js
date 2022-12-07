b5a7942e792c8f29fa5d6746a68da8935917007a
KBAEK-KIM <kkwoo8420@gmail.com>
2022-12-07 20:27:55 +0900

    update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,9 +26,33 @@ const toggleMenu = () => {
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchFormContainer = selectElement('#search-form-container');
+
+formOpenBtn.addEventListener('click', () => searchFormContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remove('activated'));
 
 // -- Close the search form popup on ESC keypress
-
+window.addEventListener('keyup', event => {
+    if(event.key === 'Escape') searchFormContainer.classList.remove('activated');
+});
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
 
+if(currentTheme){
+    bodyElement.classList.add('dark-theme');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('dark-theme');
+
+    if(bodyElement.classList.contains('dark-theme')){
+        localStorage.setItem('currentTheme', 'themeActive');
+    }else{
+        localStorage.removeItem('currentTheme');
+    };
+});
 // Swiper
